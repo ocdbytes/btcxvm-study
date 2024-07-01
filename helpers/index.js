f5e5fc4e2bdfc6946a0fd377bf71cef1048886fe
Arun Jangra <ocdbytes@Aruns-MacBook-Pro.local>
2024-07-02 01:41:14 +0530

    feat : added script and updated readme

---
+++ helpers/index.js
@@ -0,0 +1,61 @@
+import { ECPairFactory } from 'ecpair';
+import ecc from '@bitcoinerlab/secp256k1';
+import bitcoin from "bitcoinjs-lib";
+import bitcoinMessage from "bitcoinjs-message";
+
+const banner = `
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣷⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀
+⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣤⣄⠀⠀⠀⠈⠉⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢠⣶⣶⣤⡀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡆
+⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠼⣿⣿⡿⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢀⣀⣀⠀⠀⠀⠀⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⢿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠀⠀⣼⣿⣿⣿⣦⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡿
+⠸⣿⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠛⠛⠿⠟⠋⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣤⡄⠀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⡟⠀
+⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀
+⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀
+`
+
+const main = () => {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    console.log("\x1b[33m", banner, "\x1b[0m");
+    console.log("[BTC x VM Study Tools]");
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+
+    const option = process.argv[2];
+
+    const ECPair = ECPairFactory(ecc);
+    const keyPair = ECPair.makeRandom();
+    console.log("Private Key : ", keyPair.privateKey);
+    const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+    console.log("Address : ", address, "\n");
+
+    switch (option) {
+        case "1":
+            console.log(">>>> Private Key (hex) : ", keyPair.privateKey.toString('hex'));
+            console.log(">>>> Address (hex) : ", Buffer.from(address).toString('hex'));
+            return;
+        case "2":
+            const message = process.argv[3];
+            const sig = bitcoinMessage.sign(message, keyPair.privateKey, keyPair.compressed);
+            console.log("Private Key (hex) : ", keyPair.privateKey.toString('hex'));
+            console.log("Address (hex) : ", Buffer.from(address).toString('hex'));
+            console.log("\n>>>> Signature : ", sig);
+            console.log(">>>> Signature (hex) : ", sig.toString('hex'));
+            return;
+        case "--help":
+            console.log("Options : ");
+            console.log("1. Generate Random Keypair");
+            console.log("2. Sign a message");
+            return;
+        default:
+            return;
+    }
+}
+
+main();
